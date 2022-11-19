2ec18444c2294a9c8b328a9e7e895a75e59019d1
Jahn Schmidt <movsaromarov04@gmail.com>
2022-11-19 19:45:01 +0100

    file saved

--- merge-sort.js
+++ merge-sort.js
@@ -22,14 +22,9 @@ function DivideAndConquer(list) { // don't forget to input array in the corner b
 }
 
 function PutRest(mergedList, list, currentIndex) {
-    // console.log(list, currentIndex)
     for (let index = currentIndex; index < list.length; index++) {
         mergedList.push(list[index])
     }
-}
-
-const areBothDefined = (value1, value2) => {
-    return value1 != undefined && value2 != undefined
 }
 
 function CompareTwoElements(mergedList, list1, list2, index1, index2) {
@@ -40,13 +35,11 @@ function CompareTwoElements(mergedList, list1, list2, index1, index2) {
         index2++
     }
     else if (list1[index1] < list2[index2]) {
-        // console.log(smallUnit1[index1])
         mergedList.push(list1[index1])
 
         index1++
     }
     else {
-        // console.log(smallUnit2[index2])
         mergedList.push(list2[index2])
         
         index2++
@@ -55,12 +48,12 @@ function CompareTwoElements(mergedList, list1, list2, index1, index2) {
     return {1: index1, 2: index2}
 }
 
-function SortTwoArrays(list, smallUnit1, smallUnit2, index1 = 0, index2 = 0, mergedList = []) {
+function Merge(list, smallUnit1, smallUnit2, index1 = 0, index2 = 0, mergedList = []) {
     let mergedListCopy = mergedList,
     index1Copy = index1,
     index2Copy = index2
    
-    if (!areBothDefined(smallUnit1[index1], smallUnit2[index2])) {
+    if (!(smallUnit1[index1] && smallUnit2[index2])) {
         if (smallUnit1[index1]) PutRest(mergedList, smallUnit1, index1)
         else if (smallUnit2[index2]) PutRest(mergedList, smallUnit2, index2)
     }
@@ -71,8 +64,7 @@ function SortTwoArrays(list, smallUnit1, smallUnit2, index1 = 0, index2 = 0, mer
         index2Copy = container["2"]
     }
 
-
-    if (mergedList.length < smallUnit1.length + smallUnit2.length) SortTwoArrays(list, smallUnit1, smallUnit2, index1Copy, index2Copy, mergedList)
+    if (mergedList.length < smallUnit1.length + smallUnit2.length) Merge(list, smallUnit1, smallUnit2, index1Copy, index2Copy, mergedList)
     else {
         list.splice(list.indexOf(smallUnit1), 2, mergedList)
     }
@@ -81,17 +73,15 @@ function SortTwoArrays(list, smallUnit1, smallUnit2, index1 = 0, index2 = 0, mer
 function MergeSort(list) {
     const n = list[0].length,
     merges = Math.ceil(Math.log2(n))
-    // console.log(n)
     DivideAndConquer(list)
-    console.log("after DAC: ", list)
+    
     for  (let merge = 0; merge < merges; merge++) {
         for (let array = 0; array < list.length; array++) {
             if (!list[array+1]) continue
-            SortTwoArrays(list, list[array], list[array+1])
+            Merge(list, list[array], list[array+1])
         }
-        // console.log(list)
     }
 }
-// console.log(unsortedList)
+
 MergeSort(unsortedList)
 console.log("end: ", unsortedList)
